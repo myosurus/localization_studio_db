ae367eeca95251a231494636557fcdbb41b13437
myosurus <myosurus.e@gmail.com>
2025-05-19 17:02:02 +0300

    Switch to .NET Framework

--- localization_studio_db/Form1.Designer.cs
+++ localization_studio_db/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Localization
+﻿namespace localization_studio_db
 {
-    partial class Form2
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -31,9 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.Text = "Form1";
         }
 
         #endregion
     }
 }
+
--- localization_studio_db/Form1.cs
+++ localization_studio_db/Form1.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Localization
+namespace localization_studio_db
 {
-    public partial class Form2 : Form
+    public partial class Form1 : Form
     {
-        public Form2()
+        public Form1()
         {
             InitializeComponent();
         }
